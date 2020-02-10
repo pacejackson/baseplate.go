6ec50932c8f95453e973f4e6cc9188c3d323f58a
Andrew Boyle <andrew.boyle@reddit.com>
2020-02-10 10:24:32 -0800

    Add 'MonitoredRedisFactory' objects
    These Factory objects allow you to create new go-redis clients
    that are monitored by RedisSpanHooks using the span for the
    given context.

--- integrations/example_redis_test.go
+++ integrations/example_redis_test.go
@@ -11,6 +11,9 @@ import (
 
 // This example demonstrates how to use RedisSpanHook to automatically add Spans
 // around Redis commands using go-redis
+//
+// baseplate.go also provides a set of MonitoredRedisFactory objects that you can use
+// to create Redis clients with a SpanHook already attached.
 func ExampleRedisSpanHook() {
 	// variables should be properly initialized in production code
 	var (
@@ -26,6 +29,90 @@ func ExampleRedisSpanHook() {
 	ctx, _ := tracing.CreateServerSpanForContext(context.Background(), tracer, "test")
 	// Create a new client using the context for the Server Span
 	client := baseClient.WithContext(ctx)
+	// Commands should now be wrapped using Client Spans
+	client.Ping()
+}
+
+// This example demonstrates how to use a RedisClientFactory to create monitored
+// Redis clients.
+func ExampleRedisClientFactory() {
+	// variables should be properly initialized in production code
+	var tracer *tracing.Tracer
+	// Create a factory
+	factory := integrations.NewRedisClientFactory("redis", &redis.Options{Addr: ":6379"})
+	// Get a context object and a server Span, with the server Span set on the
+	// context
+	ctx, _ := tracing.CreateServerSpanForContext(context.Background(), tracer, "test")
+	// Create a new client using the context for the Server Span
+	client := factory.BuildClient(ctx)
+	// Commands should now be wrapped using Client Spans
+	client.Ping()
+}
+
+// This example demonstrates how to use a RedisClusterFactory to create monitored
+// Redis Cluster clients.
+func ExampleRedisClusterFactory() {
+	// variables should be properly initialized in production code
+	var tracer *tracing.Tracer
+	// Create a factory
+	factory := integrations.NewRedisClusterFactory(
+		"redis",
+		&redis.ClusterOptions{
+			Addrs: []string{":6379", ":6380", ":6381"},
+		},
+	)
+	// Get a context object and a server Span, with the server Span set on the
+	// context
+	ctx, _ := tracing.CreateServerSpanForContext(context.Background(), tracer, "test")
+	// Create a new client using the context for the Server Span
+	client := factory.BuildClient(ctx)
+	// Commands should now be wrapped using Client Spans
+	client.Ping()
+}
+
+// This example demonstrates how to use a RedisSentinelClientFactory to create monitored
+// Redis clients that implement failover using Redis Sentinel.
+func ExampleRedisSentinelClientFactory() {
+	// variables should be properly initialized in production code
+	var tracer *tracing.Tracer
+	// Create a factory
+	factory := integrations.NewRedisSentinelClientFactory(
+		"redis",
+		&redis.FailoverOptions{
+			MasterName:    "master",
+			SentinelAddrs: []string{":6379"},
+		},
+	)
+	// Get a context object and a server Span, with the server Span set on the
+	// context
+	ctx, _ := tracing.CreateServerSpanForContext(context.Background(), tracer, "test")
+	// Create a new client using the context for the Server Span
+	client := factory.BuildClient(ctx)
+	// Commands should now be wrapped using Client Spans
+	client.Ping()
+}
+
+// This example demonstrates how to use a RedisRingFactory to create monitored
+// Redis go-redis ring clients.
+func ExampleRedisRingFactory() {
+	// variables should be properly initialized in production code
+	var tracer *tracing.Tracer
+	// Create a factory
+	factory := integrations.NewRedisRingFactory(
+		"redis",
+		&redis.RingOptions{
+			Addrs: map[string]string{
+				"shard0": ":6379",
+				"shard1": ":6380",
+				"shard2": ":6381",
+			},
+		},
+	)
+	// Get a context object and a server Span, with the server Span set on the
+	// context
+	ctx, _ := tracing.CreateServerSpanForContext(context.Background(), tracer, "test")
+	// Create a new client using the context for the Server Span
+	client := factory.BuildClient(ctx)
 	// Commands should now be wrapped using Client Spans
 	client.Ping()
 }
